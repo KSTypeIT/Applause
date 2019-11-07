ed9f62233fda953cf5c02c2501173f58a40724d3
Kamil Sokolowski <kamil.sokolowski@vimn.com>
2019-11-07 19:52:42 +0100

    Initial Commit

---
+++ Applause/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Applause
+//
+//  Created by Sokolowski, Kamil on 07/11/2019.
+//  Copyright © 2019 TypeIT Kamil Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
