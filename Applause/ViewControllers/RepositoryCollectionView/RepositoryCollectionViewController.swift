db56c9d0091c773dbbd2b01f6463707ea8c25494
Kamil Sokolowski <kamil.sokolowski@vimn.com>
2019-11-08 15:25:23 +0100

    Search working

--- Applause/ViewControllers/RepositoryCollectionView/RepositoryCollectionViewController.swift
+++ Applause/ViewControllers/RepositoryCollectionView/RepositoryCollectionViewController.swift
@@ -16,44 +16,75 @@ private enum Constans: CGFloat {
 final class RepositoryCollectionViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+
+    let searchController = UISearchController(searchResultsController: nil)
 
     let viewModel = RepositoryCollectionViewModel()
-    
+
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    var isFiltering: Bool {
+        return searchController.isActive && !isSearchBarEmpty
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        defineLayout()
+        searchControllerSetup()
+        defineCollectionViewLayout()
         collectionView.register(UINib(nibName: "RepositoryCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "kRepositoryCell")
+
         viewModel.delegate = self
         viewModel.viewDidLoad()
     }
 
-    private func defineLayout() {
+    private func defineCollectionViewLayout() {
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         let width = UIScreen.main.bounds.width
         layout.itemSize = CGSize(width: width, height: Constans.cellHeight.rawValue)
         layout.minimumLineSpacing = Constans.minimumLineSpacing.rawValue
         collectionView!.collectionViewLayout = layout
     }
+
+    private func searchControllerSetup() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
 }
 
 extension RepositoryCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegate, RepositoryCollectionViewModelDelegate {
-    func repositoriesDownloaded() {
+    func repositoriesReload() {
         collectionView.reloadData()
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        if isFiltering {
+            return viewModel.getFilteredRepositories().count
+        }
         return viewModel.getRepositories().count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: RepositoryCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "kRepositoryCell", for: indexPath) as! RepositoryCollectionViewCell
-        cell.setupWith(repository: viewModel.getRepositoryAtIndex(indexPath: indexPath))
+        cell.setupWith(repository: viewModel.getRepositoryAtIndex(indexPath: indexPath, filtered: isFiltering))
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
 
+    }
+}
+
+extension RepositoryCollectionViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        viewModel.findRepository(searchBar.text!)
     }
 }
 
--- Applause/ViewControllers/RepositoryCollectionView/RepositoryCollectionViewModel.swift
+++ Applause/ViewControllers/RepositoryCollectionView/RepositoryCollectionViewModel.swift
@@ -9,21 +9,24 @@
 import Foundation
 
 protocol RepositoryCollectionViewModelDelegate: AnyObject {
-    func repositoriesDownloaded()
+    func repositoriesReload()
 }
 
 protocol RepositoryCollectionViewModelProtocol {
     func viewDidLoad()
     func getRepositories() -> [Repository]
+    func getFilteredRepositories() -> [Repository] 
     func getRepositoryAtIndex(indexPath: IndexPath) -> Repository
-    func findRepository(_ searchString: String) -> Repository
+    func findRepository(_ searchString: String)
 }
 
 final class RepositoryCollectionViewModel {
+
     weak var delegate: RepositoryCollectionViewModelDelegate?
 
     private let dataProvider: DataProviding
     private var repositories: [Repository] = []
+    private var filteredRepositories: [Repository] = []
 
     init (dataProvider: DataProviding = DataProvider()){
         self.dataProvider = dataProvider
@@ -33,7 +36,7 @@ final class RepositoryCollectionViewModel {
         dataProvider.requestAllRepositories(completionHandler: { [weak self] repositories in
             self?.repositories = repositories
              DispatchQueue.main.async {
-                self?.delegate?.repositoriesDownloaded()
+                self?.delegate?.repositoriesReload()
              }
         }, errorHandler: { _ in
             //
@@ -44,11 +47,21 @@ final class RepositoryCollectionViewModel {
         return self.repositories
     }
 
-    public func getRepositoryAtIndex(indexPath: IndexPath) -> Repository {
-        self.repositories[indexPath.row]
+    public func getFilteredRepositories() -> [Repository] {
+           return self.filteredRepositories
+       }
+
+    public func getRepositoryAtIndex(indexPath: IndexPath, filtered: Bool) -> Repository {
+        if filtered {
+           return self.filteredRepositories[indexPath.row]
+        }
+        return self.repositories[indexPath.row]
     }
 
-    public func findRepository(_ searchString: String) -> Repository {
-        Repository(id: 1, name: "", url: "", size: 12)
+    public func findRepository(_ searchString: String) {
+        self.filteredRepositories = self.repositories.filter {
+            $0.name.range(of: searchString, options: .caseInsensitive) != nil
+        }
+        self.delegate?.repositoriesReload()
     }
 }
